a6d42c12962f258ca4f546fa95a0e4540a7e6595
Owen Astrachan <ola@cs.duke.edu>
2019-09-23 16:22:51 -0400

    initial commit

---
+++ src/WordGram.java
@@ -0,0 +1,91 @@
+
+/**
+ * A WordGram represents a sequence of strings
+ * just as a String represents a sequence of characters
+ * 
+ * @author YOUR NAME HERE
+ *
+ */
+public class WordGram {
+	
+	private String[] myWords;   
+	private String myToString;  // cached string
+	private int myHash;         // cached hash value
+
+	/**
+	 * Create WordGram by creating instance variable myWords and copying
+	 * size strings from source starting at index start
+	 * @param source is array of strings from which copying occurs
+	 * @param start starting index in source for strings to be copied
+	 * @param size the number of strings copied
+	 */
+	public WordGram(String[] source, int start, int size) {
+		myWords = new String[size];
+		myToString = null;
+		myHash = 0;
+
+		// TODO: initialize all instance variable
+	}
+
+	/**
+	 * Return string at specific index in this WordGram
+	 * @param index in range [0..length() ) for string 
+	 * @return string at index
+	 */
+	public String wordAt(int index) {
+		if (index < 0 || index >= myWords.length) {
+			throw new IndexOutOfBoundsException("bad index in wordAt "+index);
+		}
+		return myWords[index];
+	}
+
+	/**
+	 * Complete this comment
+	 * @return
+	 */
+	public int length(){
+		// TODO: change this
+		return 0;
+	}
+
+
+	/**
+	 * Complete appropriate comment here
+	 * @param o
+	 * @return
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (! (o instanceof WordGram) || o == null){
+			return false;
+		}
+		// TODO: Complete this method
+
+		return true;
+	}
+
+	@Override
+	public int hashCode(){
+		// TODO: complete this method
+		return myHash;
+	}
+	
+
+	/**
+	 * Create and complete this comment
+	 * @param last is last String of returned WordGram
+	 * @return
+	 */
+	public WordGram shiftAdd(String last) {
+		WordGram wg = new WordGram(myWords,0,myWords.length);
+		// TODO: Complete this method
+
+		return wg;
+	}
+
+	@Override
+	public String toString(){
+		// TODO: Complete this method
+		return myToString;
+	}
+}
